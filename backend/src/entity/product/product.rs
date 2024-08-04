9a5585ae8e3fd6a0bab2b2bd65f0c0a62197e6d9
penysho <penysho@gmail.com>
2024-08-04 16:41:06 +0900

    feat: 商品ドメイン初期定義

---
+++ backend/src/entity/product/product.rs
@@ -0,0 +1,33 @@
+pub struct Product {
+    id: String,
+    name: String,
+    price: u32,
+    description: String,
+}
+
+impl Product {
+    pub fn new(id: String, name: String, price: u32, description: String) -> Self {
+        Product {
+            id,
+            name,
+            price,
+            description,
+        }
+    }
+
+    pub fn get_id(&self) -> &str {
+        &self.id
+    }
+
+    pub fn get_name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn get_price(&self) -> u32 {
+        self.price
+    }
+
+    pub fn get_description(&self) -> &str {
+        &self.description
+    }
+}
