87ac263eee981ab69eefe1b4bfa3a5a79990bfb6
penysho <penysho@gmail.com>
2024-09-23 17:33:40 +0900

    feat: ProductPresenterにおけるconverterへのロジック切り出し

--- backend/src/interface/presenter/product/schema.rs
+++ backend/src/interface/presenter/product/schema.rs
@@ -3,16 +3,7 @@ use chrono::{DateTime, Utc};
 use derive_more::{Display, Error};
 use serde::{Deserialize, Serialize};
 
-use crate::{
-    domain::{
-        media::media::{Media, MediaStatus},
-        product::{
-            product::{Product, ProductStatus},
-            variant::variant::Variant,
-        },
-    },
-    interface::presenter::common::exception::GenericResponseError,
-};
+use crate::interface::presenter::common::exception::GenericResponseError;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub(super) enum ProductStatusEnum {
@@ -59,66 +50,6 @@ pub(super) struct MediaSchema {
     pub(super) src: Option<String>,
     pub(super) created_at: DateTime<Utc>,
     pub(super) updated_at: DateTime<Utc>,
-}
-
-impl ProductSchema {
-    pub fn to_schema(product: Product, media: Vec<Media>) -> Self {
-        ProductSchema {
-            id: product.id().to_string(),
-            name: product.name().to_string(),
-            description: product.description().to_string(),
-            status: match product.status() {
-                ProductStatus::Active => ProductStatusEnum::Active,
-                ProductStatus::Inactive => ProductStatusEnum::Inactive,
-                ProductStatus::Draft => ProductStatusEnum::Draft,
-            },
-            category_id: product.category_id().to_owned(),
-            media: media
-                .into_iter()
-                .map(|media| MediaSchema::from(media))
-                .collect(),
-            variants: product
-                .variants()
-                .iter()
-                .map(|variant| VariantSchema::from(variant))
-                .collect(),
-        }
-    }
-}
-
-impl From<Media> for MediaSchema {
-    fn from(media: Media) -> Self {
-        MediaSchema {
-            id: media.id().to_string(),
-            status: match media.status() {
-                MediaStatus::Active => MediaStatusEnum::Active,
-                MediaStatus::Inactive => MediaStatusEnum::Inactive,
-                MediaStatus::InPreparation => MediaStatusEnum::InPreparation,
-            },
-            alt: media.alt().to_owned(),
-            src: media.published_src().as_ref().map(|s| s.value().to_owned()),
-            created_at: media.created_at().to_owned(),
-            updated_at: media.updated_at().to_owned(),
-        }
-    }
-}
-
-impl From<&Variant> for VariantSchema {
-    fn from(variant: &Variant) -> Self {
-        VariantSchema {
-            id: variant.id().to_string(),
-            price: *(variant.price()),
-            sku: variant.sku().as_ref().map(|sku| sku.value().to_owned()),
-            barcode: variant
-                .barcode()
-                .as_ref()
-                .map(|barcode| barcode.value().to_owned()),
-            inventory_quantity: variant.inventory_quantity().to_owned(),
-            list_order: variant.list_order().to_owned(),
-            created_at: variant.created_at().to_owned(),
-            updated_at: variant.updated_at().to_owned(),
-        }
-    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
