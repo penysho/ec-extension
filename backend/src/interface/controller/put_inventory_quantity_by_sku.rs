64a3b3155646bd98ecb092f9c8661dc87ca51c47
penysho <penysho@gmail.com>
2024-10-06 23:08:39 +0900

    feat: put_inventory_quantity_by_skuのUT実装

--- backend/src/interface/controller/put_inventory_quantity_by_sku.rs
+++ backend/src/interface/controller/put_inventory_quantity_by_sku.rs
@@ -2,7 +2,7 @@ use actix_web::{
     web::{self, Path},
     Responder,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::{
     domain::{
@@ -24,7 +24,7 @@ use crate::{
 
 use super::controller::Controller;
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct PutInventoryQuantityBySkuRequest {
     name: String,
     reason: String,
@@ -109,5 +109,162 @@ impl Controller {
             .await;
 
         presenter.present_put_inventory(result).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use crate::domain::inventory_level::inventory_level::InventoryLevel;
+    use crate::domain::inventory_level::quantity::quantity::{InventoryType, Quantity};
+    use crate::infrastructure::router::actix_router;
+    use crate::interface::controller::interact_provider_interface::MockInteractProvider;
+    use crate::usecase::interactor::inventory_interactor_interface::{
+        InventoryInteractor, MockInventoryInteractor,
+    };
+
+    use super::*;
+    use actix_http::Request;
+    use actix_web::dev::{Service, ServiceResponse};
+    use actix_web::web;
+    use actix_web::{http::StatusCode, test, App, Error};
+    use mockall::predicate::eq;
+
+    const BASE_URL: &'static str = "/ec-extension/inventories/quantities/sku";
+
+    async fn setup(
+        interactor: MockInventoryInteractor,
+    ) -> impl Service<Request, Response = ServiceResponse, Error = Error> {
+        // Configure the InteractProvider mock
+        let mut interact_provider = MockInteractProvider::new();
+        interact_provider
+            .expect_provide_inventory_interactor()
+            .return_once(move || Box::new(interactor) as Box<dyn InventoryInteractor>);
+
+        let controller = web::Data::new(Arc::new(Controller::new(Box::new(interact_provider))));
+
+        // Create an application for testing
+        test::init_service(
+            App::new()
+                .app_data(controller)
+                .configure(actix_router::configure_routes),
+        )
+        .await
+    }
+
+    #[actix_web::test]
+    async fn test_put_inventory_quantity_by_sku_success() {
+        let sku = "test-sku-1";
+        let delta = 10;
+        let ledger_document_uri: Option<LedgerDocumentUri> = None;
+        let location_id = "location_id";
+
+        let mut interactor = MockInventoryInteractor::new();
+        interactor
+            .expect_allocate_inventory_by_sku_with_location()
+            .with(
+                eq(Sku::new(sku).unwrap()),
+                eq(InventoryType::Available),
+                eq(InventoryChangeReason::Correction),
+                eq(delta),
+                eq(ledger_document_uri),
+                eq(location_id.to_string()),
+            )
+            .returning(|_, _, _, _, _, _| {
+                InventoryLevel::new(
+                    "0",
+                    "0",
+                    "location_id",
+                    vec![
+                        Quantity::new(10, InventoryType::Available).unwrap(),
+                        Quantity::new(20, InventoryType::Committed).unwrap(),
+                        Quantity::new(30, InventoryType::Incoming).unwrap(),
+                        Quantity::new(40, InventoryType::Reserved).unwrap(),
+                        Quantity::new(50, InventoryType::SafetyStock).unwrap(),
+                        Quantity::new(60, InventoryType::Damaged).unwrap(),
+                    ],
+                )
+            });
+
+        let req = test::TestRequest::put()
+            .uri(&format!("{BASE_URL}/{sku}"))
+            .set_json(PutInventoryQuantityBySkuRequest {
+                name: "available".to_string(),
+                reason: "correction".to_string(),
+                delta: delta,
+                ledger_document_uri: None,
+                location_id: location_id.to_string(),
+            })
+            .to_request();
+        let resp: ServiceResponse = test::call_service(&setup(interactor).await, req).await;
+
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_web::test]
+    async fn test_put_inventory_quantity_by_sku_not_found() {
+        let mut interactor = MockInventoryInteractor::new();
+        interactor
+            .expect_allocate_inventory_by_sku_with_location()
+            .returning(|_, _, _, _, _, _| Err(DomainError::NotFound));
+
+        let req = test::TestRequest::put()
+            .uri(&format!("{BASE_URL}/test-sku-1"))
+            .set_json(PutInventoryQuantityBySkuRequest {
+                name: "available".to_string(),
+                reason: "correction".to_string(),
+                delta: 2,
+                ledger_document_uri: None,
+                location_id: "location_id".to_string(),
+            })
+            .to_request();
+        let resp: ServiceResponse = test::call_service(&setup(interactor).await, req).await;
+
+        assert_eq!(resp.status(), StatusCode::NOT_FOUND);
+    }
+
+    #[actix_web::test]
+    async fn test_put_inventory_quantity_by_sku_bad_request() {
+        let mut interactor = MockInventoryInteractor::new();
+        interactor
+            .expect_allocate_inventory_by_sku_with_location()
+            .returning(|_, _, _, _, _, _| Err(DomainError::ValidationError));
+
+        let req = test::TestRequest::put()
+            .uri(&format!("{BASE_URL}/test-sku-1"))
+            .set_json(PutInventoryQuantityBySkuRequest {
+                name: "available".to_string(),
+                reason: "correction".to_string(),
+                delta: 2,
+                ledger_document_uri: None,
+                location_id: "location_id".to_string(),
+            })
+            .to_request();
+        let resp: ServiceResponse = test::call_service(&setup(interactor).await, req).await;
+
+        assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[actix_web::test]
+    async fn test_put_inventory_quantity_by_sku_service_unavailable() {
+        let mut interactor = MockInventoryInteractor::new();
+        interactor
+            .expect_allocate_inventory_by_sku_with_location()
+            .returning(|_, _, _, _, _, _| Err(DomainError::SystemError));
+
+        let req = test::TestRequest::put()
+            .uri(&format!("{BASE_URL}/test-sku-1"))
+            .set_json(PutInventoryQuantityBySkuRequest {
+                name: "available".to_string(),
+                reason: "correction".to_string(),
+                delta: 2,
+                ledger_document_uri: None,
+                location_id: "location_id".to_string(),
+            })
+            .to_request();
+        let resp: ServiceResponse = test::call_service(&setup(interactor).await, req).await;
+
+        assert_eq!(resp.status(), StatusCode::SERVICE_UNAVAILABLE);
     }
 }
