af44a8c2ea701118b263bc30539bf258d6e422ee
penysho <penysho@gmail.com>
2024-10-19 13:33:40 +0900

    feat: repositoryにおけるcurrency_codeを構造体として定義

--- backend/src/infrastructure/ec/shopify/repository/schema/money.rs
+++ backend/src/infrastructure/ec/shopify/repository/schema/money.rs
@@ -17,13 +17,7 @@ impl MoneyBagNode {
 impl MoneyNode {
     pub fn to_domain(self) -> Result<MoneyBag, DomainError> {
         let amount = Money::new(self.amount.parse::<f64>().unwrap_or(0.0))?;
-        let currency_code = match self.currency_code.as_str() {
-            "USD" => Ok(CurrencyCode::USD),
-            "EUR" => Ok(CurrencyCode::EUR),
-            "GBP" => Ok(CurrencyCode::GBP),
-            "JPY" => Ok(CurrencyCode::JPY),
-            _ => Err(DomainError::ConversionError),
-        }?;
+        let currency_code = self.currency_code.to_domain()?;
         MoneyBag::new(currency_code, amount)
     }
 }
@@ -38,5 +32,20 @@ pub struct MoneyBagNode {
 pub struct MoneyNode {
     pub amount: String,
     #[serde(rename = "currencyCode")]
-    pub currency_code: String,
+    pub currency_code: CurrencyCodeNode,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CurrencyCodeNode(String);
+
+impl CurrencyCodeNode {
+    pub fn to_domain(self) -> Result<CurrencyCode, DomainError> {
+        match self.0.as_str() {
+            "USD" => Ok(CurrencyCode::USD),
+            "EUR" => Ok(CurrencyCode::EUR),
+            "GBP" => Ok(CurrencyCode::GBP),
+            "JPY" => Ok(CurrencyCode::JPY),
+            _ => Err(DomainError::ConversionError),
+        }
+    }
 }
