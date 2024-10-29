4a63a0a4e0d88c0a4b5dcf1b2ef25b3f2243387f
penysho <penysho@gmail.com>
2024-10-29 23:24:29 +0900

    feat: LocationRepository::find_locationsの実装

--- backend/src/usecase/repository/location_repository_interface.rs
+++ backend/src/usecase/repository/location_repository_interface.rs
@@ -1,8 +1,17 @@
 use async_trait::async_trait;
 
-use crate::domain::{error::error::DomainError, location::location::Id as LocationId};
+use crate::domain::{
+    error::error::DomainError,
+    location::location::{Id as LocationId, Location},
+};
 
 #[async_trait]
 pub trait LocationRepository: Send + Sync {
     async fn find_all_location_ids(&self) -> Result<Vec<LocationId>, DomainError>;
+
+    async fn find_locations(
+        &self,
+        limit: &Option<u32>,
+        offset: &Option<u32>,
+    ) -> Result<Vec<Location>, DomainError>;
 }
