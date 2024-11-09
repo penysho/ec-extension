4f2f81248dcefd35562cc16acf795e8b6fee467f
penysho <penysho@gmail.com>
2024-11-09 19:37:19 +0900

    feat: PresenterにおけるProductSchemaのフィールド追加

--- backend/src/interface/presenter/product/schema.rs
+++ backend/src/interface/presenter/product/schema.rs
@@ -9,13 +9,6 @@ use crate::usecase::query_service::dto::product::ProductDTO;
 use crate::{define_error_response, interface::presenter::common::exception::ErrorResponseBuilder};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum ProductStatusEnum {
-    Active,
-    Inactive,
-    Draft,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
 pub struct ProductSchema {
     pub(super) id: String,
     pub(super) name: String,
@@ -27,16 +20,37 @@ pub struct ProductSchema {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
+pub enum ProductStatusEnum {
+    Active,
+    Inactive,
+    Draft,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct VariantSchema {
     pub(super) id: String,
     pub(super) name: Option<String>,
-    pub(super) price: u32,
     pub(super) sku: Option<String>,
     pub(super) barcode: Option<String>,
-    pub(super) inventory_quantity: Option<u32>,
+    pub(super) available_for_sale: bool,
     pub(super) list_order: u8,
+
+    pub(super) inventory_item_id: String,
+    pub(super) inventory_policy: InventoryPolicyEnum,
+    pub(super) inventory_quantity: Option<u32>,
+
+    pub(super) price: f64,
+    pub(super) taxable: bool,
+    pub(super) tax_code: Option<String>,
+
     pub(super) created_at: DateTime<Utc>,
     pub(super) updated_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum InventoryPolicyEnum {
+    Deny,
+    Continue,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
