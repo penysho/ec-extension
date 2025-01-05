012b42b223058ee0c8102375fdcbbe7f5f7b785d
penysho <penysho@gmail.com>
2025-01-05 04:42:44 +0000

    docs: トレイトへのドキュメンテーションコメント追加

--- backend/src/infrastructure/db/transaction_manager_interface.rs
+++ backend/src/infrastructure/db/transaction_manager_interface.rs
@@ -5,9 +5,12 @@ use async_trait::async_trait;
 pub trait TransactionManager {
     type Transaction;
 
+    /// Initiating a transaction.
     async fn begin(&self) -> Result<Self::Transaction, DomainError>;
 
+    /// Commit transaction.
     async fn commit(&self, transaction: Self::Transaction) -> Result<(), DomainError>;
 
+    /// Roll back a transaction.
     async fn rollback(&self, transaction: Self::Transaction) -> Result<(), DomainError>;
 }
