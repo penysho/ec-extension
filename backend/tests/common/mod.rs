c274b9988a9c7d31a76d6afd14e51edb4ec3bbec
penysho <penysho@gmail.com>
2025-01-11 07:46:47 +0000

    test: ITテストのルーター定義修正

--- backend/tests/common/mod.rs
+++ backend/tests/common/mod.rs
@@ -3,8 +3,11 @@ use actix_web::{
     dev::{Service, ServiceResponse},
     test, App, Error,
 };
-use backend::infrastructure::router::actix_router;
+use backend::infrastructure::{
+    module::interact_provider_impl::InteractProviderImpl, router::actix_router,
+};
 
 pub async fn setup() -> impl Service<Request, Response = ServiceResponse, Error = Error> {
-    test::init_service(App::new().configure(actix_router::configure_routes)).await
+    test::init_service(App::new().configure(actix_router::configure_routes::<InteractProviderImpl>))
+        .await
 }
