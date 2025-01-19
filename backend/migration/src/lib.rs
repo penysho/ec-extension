a9211bf316e8d87ea073b35278584846d34455ca
penysho <penysho@gmail.com>
2025-01-19 08:01:32 +0000

    feat: Roleテーブルの定義

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20250119_074443_create_user;
+mod m20250119_075309_create_role;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20250119_074443_create_user::Migration)]
+        vec![
+            Box::new(m20250119_074443_create_user::Migration),
+            Box::new(m20250119_075309_create_role::Migration),
+        ]
     }
 }
---
+++ backend/migration/src/m20250119_075309_create_role.rs
@@ -0,0 +1,39 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Role::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Role::Id))
+                    .col(string(Role::Name))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        todo!();
+
+        manager
+            .drop_table(Table::drop().table(Role::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Role {
+    Table,
+    Id,
+    Name,
+}
