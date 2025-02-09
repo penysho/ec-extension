6709173206b0dc1cf7757d5b9b04235478cdc1bb
penysho <penysho@gmail.com>
2025-02-09 04:50:15 +0000

    feat: seed修正

--- backend/migration/seed/role_resource_permission.sql
+++ backend/migration/seed/role_resource_permission.sql
@@ -1,11 +1,11 @@
-INSERT INTO "role_resouce_permission" (id, role_id, resource_id, permission_id)
+INSERT INTO "role_resource_permission" (id, role_id, resource_id, permission_id)
 VALUES (1, 1, 1, 1);
-INSERT INTO "role_resouce_permission" (id, role_id, resource_id, permission_id)
+INSERT INTO "role_resource_permission" (id, role_id, resource_id, permission_id)
 VALUES (2, 1, 1, 2);
-INSERT INTO "role_resouce_permission" (id, role_id, resource_id, permission_id)
+INSERT INTO "role_resource_permission" (id, role_id, resource_id, permission_id)
 VALUES (3, 1, 1, 3);
-INSERT INTO "role_resouce_permission" (id, role_id, resource_id, permission_id)
+INSERT INTO "role_resource_permission" (id, role_id, resource_id, permission_id)
 VALUES (4, 1, 1, 4);
-INSERT INTO "role_resouce_permission" (id, role_id, resource_id, permission_id)
+INSERT INTO "role_resource_permission" (id, role_id, resource_id, permission_id)
 VALUES (5, 1, 1, 5);
 
