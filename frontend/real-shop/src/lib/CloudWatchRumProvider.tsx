d3dcf48b5c54d4645edc241492885bed23b61d88
penysho <penysho@gmail.com>
2025-05-23 23:05:27 +0900

    feat: CloudWatchRUM設定

---
+++ frontend/real-shop/src/lib/CloudWatchRumProvider.tsx
@@ -0,0 +1,27 @@
+import { AwsRum, AwsRumConfig } from "aws-rum-web"
+
+const CloudWatchRumProvider = ({ children }: { children: React.ReactNode }) => {
+  try {
+    const config: AwsRumConfig = {
+      sessionSampleRate: 1,
+      endpoint: "https://dataplane.rum.ap-northeast-1.amazonaws.com",
+      telemetries: ["performance", "errors", "http"],
+      allowCookies: true,
+      enableXRay: true,
+      signing: true, // If you have a public resource policy and wish to send unsigned requests please set this to false
+    }
+
+    const APPLICATION_ID: string = process.env.CLOUDWATCH_RUM_APPLICATION_ID!
+    const APPLICATION_VERSION: string = process.env.CLOUDWATCH_RUM_VERSION!
+    const APPLICATION_REGION: string = process.env.CLOUDWATCH_RUM_REGION!
+
+    new AwsRum(APPLICATION_ID, APPLICATION_VERSION, APPLICATION_REGION, config)
+  } catch (error) {
+    // Ignore errors thrown during CloudWatch RUM web client initialization
+    console.error(error)
+  }
+
+  return <>{children}</>
+}
+
+export default CloudWatchRumProvider
