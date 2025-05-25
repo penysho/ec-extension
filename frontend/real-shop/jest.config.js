4425a1ab8b4c17db99c7b24030245024e6a7a049
penysho <penysho@gmail.com>
2025-05-25 11:33:26 +0900

    feat: 閾値を設定しない

--- frontend/real-shop/jest.config.js
+++ frontend/real-shop/jest.config.js
@@ -29,14 +29,14 @@ module.exports = {
   ],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html"],
-  coverageThreshold: {
-    global: {
-      branches: 70,
-      functions: 70,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 70,
+  //     functions: 70,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
   transform: {
     // https://swc.rs/docs/usage/jest
     "^.+\\.(t|j)sx?$": [
